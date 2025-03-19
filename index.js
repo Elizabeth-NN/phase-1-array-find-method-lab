8b394a3ccf8d39f81dbb6ebe30ea815e9a296d09
Elizabeth Njuguna <elizabethnjuguna217@gmail.com>
2025-03-19 12:50:42 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(records) {
+    // Use the find() method to locate the first record with result "W"
+    const winningRecord = records.find(record => record.result === "W");
+  
+    // If a winning record is found, return the year; otherwise, return undefined
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  const record = [
+    { year: 2018, result: "L" },
+    { year: 2017, result: "W" },
+    { year: 2016, result: "N/A" }
+  ];
+  
+  console.log(superbowlWin(record));
